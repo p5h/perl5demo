7b26ef7988e1cc800b4473c17bee56e07ea0a631
Yves Orton <demerphq@gmail.com>
2016-11-09 10:25:47 +0100

    prevent sv_dump() from being called from within sv_dump()
    Note this only protects the sv_dump() wrapper call, which
    may be called from debugging code which might actually be
    used by sv_dump() itself. In general this doesn't produce
    an infinite loop, but it could, and it certainly makes
    the output less useful.
    
    So this flag var prevents it from being called twice.
    If there was a bitmap I could use instead of a full interpvar
    I would.

--- dump.c
+++ dump.c
@@ -2435,10 +2435,18 @@ For an example of its output, see L<Devel::Peek>.
 void
 Perl_sv_dump(pTHX_ SV *sv)
 {
+    PERL_ARGS_ASSERT_SV_DUMP;
+    if (PL_in_sv_dump)
+        return;
+    else
+        PL_in_sv_dump = 1;
+
     if (sv && SvROK(sv))
 	do_sv_dump(0, Perl_debug_log, sv, 0, 4, 0, 0);
     else
 	do_sv_dump(0, Perl_debug_log, sv, 0, 0, 0, 0);
+
+    PL_in_sv_dump = 0;
 }
 
 int
